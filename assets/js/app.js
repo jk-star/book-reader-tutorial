eb10128503d677d3f2b038713bd7519ee57542b4
jyoti singh <jyotisinghcse2015@gmail.com>
2026-07-21 16:05:37 +0530

    feat: add live search to library

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,81 @@
 const bookGrid = document.getElementById("bookGrid");
+const searchInput = document.getElementById("searchInput");
+
+let books = [];
 
 fetch("assets/data/books.json")
-    .then(response => {
+    .then(res => res.json())
+    .then(data => {
 
-        if (!response.ok) {
+        books = data;
 
-            throw new Error("books.json not found");
-
-        }
-
-        return response.json();
-
-    })
-
-    .then(books => {
-
-        bookGrid.innerHTML = "";
-
-        books.forEach(book => {
-
-            bookGrid.innerHTML += `
-
-            <article class="book-card">
-
-                <div class="book-cover">
-
-                    <img
-                        src="${book.cover}"
-                        alt="${book.title}"
-                    >
-
-                </div>
-
-                <h2>${book.title}</h2>
-
-                <p>${book.author}</p>
-
-                <p>${book.total_chapters} Chapters</p>
-
-                <button onclick="openBook('${book.slug}')">
-
-                    Continue Reading
-
-                </button>
-
-            </article>
-
-            `;
-
-        });
-
-    })
-
-    .catch(error => {
-
-        console.error(error);
-
-        bookGrid.innerHTML = `
-
-            <h2>Unable to load books.</h2>
-
-            <p>${error.message}</p>
-
-        `;
+        renderBooks(books);
 
     });
+
+function renderBooks(bookList) {
+
+    bookGrid.innerHTML = "";
+
+    if (bookList.length === 0) {
+        bookGrid.innerHTML = `
+            <div class="empty-books">
+                <h2>📚 No Books Found</h2>
+                <p>Try another keyword.</p>
+            </div>
+        `;
+        return;
+    }
+
+    bookList.forEach(book => {
+        bookGrid.innerHTML += `
+        <article class="book-card">
+            <div class="book-cover">
+                <img src="${book.cover}" alt="${book.title}">
+            </div>
+
+            <h2>${book.title}</h2>
+
+            <p>${book.author}</p>
+
+            <p>${book.category}</p>
+
+            <p>${book.total_chapters} Chapters</p>
+
+            <button onclick="openBook('${book.slug}')">
+                Continue Reading
+            </button>
+        </article>`;
+    });
+
+}
+
+
+searchInput.addEventListener("input", function () {
+
+    const keyword = this.value.toLowerCase();
+
+    const filteredBooks = books.filter(book => {
+
+        return (
+
+            book.title.toLowerCase().includes(keyword)
+
+            ||
+
+            book.author.toLowerCase().includes(keyword)
+
+            ||
+
+            book.category.toLowerCase().includes(keyword)
+
+        );
+
+    });
+
+    renderBooks(filteredBooks);
+
+});
 
 function openBook(slug) {
 
